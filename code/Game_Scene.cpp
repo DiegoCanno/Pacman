4931b3202269fac20039e7ef46683e305f1a295a
DiegoCanno <diego.canno@gmail.com>
2021-07-10 19:24:47 +0200

    Quitados elementos sobrantes

--- code/Game_Scene.cpp
+++ code/Game_Scene.cpp
@@ -530,38 +530,14 @@ namespace example
 
                     if(RandomNumber==1){
                         phantom->set_speed_x(200);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                     }
                     else if(RandomNumber==2){
                         phantom->set_speed_y(-200);
                     }
-=======
+
                     }
-                    else if(RandomNumber==2){
-                        phantom->set_speed_y(-200);
-                    }
->>>>>>> parent of f9f3450 (Prueba)
-=======
-                    }
-                    else if(RandomNumber==2){
-                        phantom->set_speed_y(-200);
-                    }
->>>>>>> parent of f9f3450 (Prueba)
-=======
-                    }
-                    else if(RandomNumber==2){
-                        phantom->set_speed_y(-200);
-                    }
->>>>>>> parent of f9f3450 (Prueba)
-=======
-                    }
-                    else if(RandomNumber==2){
-                        phantom->set_speed_y(-200);
-                    }
->>>>>>> parent of f9f3450 (Prueba)
+                
                     else if(RandomNumber==3){
                         phantom->set_speed_x(-200);
                     }
